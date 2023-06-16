4672acebb476835d26f1715e3372f587841fff71
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2023-06-16 21:47:48 +0100

    Displayed tasks to dashboard

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,4 +1,7 @@
-const welcomeUser = document.querySelector('.welcome-user')
+// =================== User/Todo Information ================== //
+
+const welcomeUserSm = document.querySelector('.sm-welcome')
+const welcomeUserLg = document.querySelector('.lg-welcome')
 
 // Function to retrieve the user from the localStorage using the stored userIndex
 const getUserFromLocalStorage = () => {
@@ -9,8 +12,8 @@ const getUserFromLocalStorage = () => {
       if (!isNaN(index) && index >= 0 && index < users.length) {
         return users[index];
     }
-}
-return null;
+  }
+  return null;
 }
 
 // Trigger the getUserFromLocalStorage() function
@@ -18,8 +21,86 @@ const user = getUserFromLocalStorage();
 
 if (user !== null) {
     console.log(user);
-    welcomeUser.innerHTML = `<h5 class="fw-bold welcome-text">Welcome, ${user.FirstName}</h5>`
-  } else {
-    console.log('User not found.');
+    welcomeUserSm.innerHTML = `<h5 class="fw-bold welcome-text-sm">Welcome, ${user.FirstName}</h5>`
+    welcomeUserLg.innerHTML = `<h5 class="fw-bold welcome-text">Welcome, ${user.FirstName}</h5>`
+} else {
+  console.log('User not found.');
+  window.location.href = 'signIn.html'
+}
+
+
+// ==================== Todo Schema ===================== //
+
+// Variable Declarations
+const addTodoBtn = document.getElementById('addTodoBtn')
+let allTodo = []
+
+// Function to add todo and set to localStorage
+const addTodo = () => {
+  let todoSchema = {
+    todoTitle: title.value,
+    todoCategory: category.value,
+    todoDescription: description.value,
+    todoTime: time.value
   }
-  
+
+  if (localStorage.userTodo) {
+    allTodo = JSON.parse(localStorage.getItem('userTodo'))
+    allTodo.push(todoSchema)
+    localStorage.setItem('userTodo', JSON.stringify(allTodo))
+    console.log('Tasks Added');
+    window.location.href = 'dashboard.html'
+  } else {
+    allTodo.push(todoSchema)
+    localStorage.setItem('userTodo', JSON.stringify(allTodo))
+    console.log('Tasks Added');
+    window.location.href = 'dashboard.html'
+  }
+}
+
+// Button to trigger addTodo() function
+addTodoBtn.addEventListener('click', () => {
+  if (title.value != '' && category.value != '' && description.value != '' && time.value != '' && isNaN(title.value) && isNaN(category.value) && isNaN(description.value)) {
+    addTodo();
+  } else {
+    console.log('Enter Valid details');
+  }
+})
+
+// ============ Display Tasks ============ //
+
+const tasks = JSON.parse(localStorage.getItem('userTodo'))
+const noTask = document.querySelector('.no-task')
+const taskImg = document.querySelector('.task-image')
+
+if (tasks != null) {
+  console.log(tasks);
+  noTask.classList.add('d-none')
+  tasks.map((task, index) => {
+    displayTasks.innerHTML += `
+    <div class="border-bottom py-2">
+      <div class="col-12">
+        <div class="text-light">
+          <h4 class="text-warning">${index + 1}</h4
+          <h5 id="displayTitle">Title: ${task.todoTitle}</h5>
+          <p id="displayCategory">Category: ${task.todoCategory}</p>
+          <p id="displayDescription">Description: ${task.todoDescription}</p>
+          <i id="displayTime">Date/Time: ${task.todoTime}</i>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="row todo-btn">
+          <div class="col-6">
+            <button class="btn btn-warning">Edit</button>
+          </div>
+          <div class="col-6">
+            <button class="btn btn-danger float-end">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+  })
+} else {
+  taskImg.classList.remove('d-md-block')
+}
--- script/signIn.js
+++ script/signIn.js
@@ -1,39 +1,47 @@
 const signInBtn = document.getElementById('signInBtn')
-const allUsers = JSON.parse(localStorage.getItem('localUsers'))
 
 signInBtn.addEventListener('click', ()=> {
     
-    // Function to check if the provided email and password match a user in the array
-    const signIn = (email, password) => {
-        // The array.findIndex() line to search for a user with the index and the provided email
-        const userIndex = allUsers.findIndex(user => user.Email == email)
-    
-        // If a user is found and the password matches, return true
-        if (userIndex !== -1 && allUsers[userIndex].Password == password) {
-            localStorage.setItem('userIndex', userIndex)
-            return true;
+    if (localStorage.localUsers) {
+        const allUsers = JSON.parse(localStorage.getItem('localUsers'))
+        // Function to check if the provided email and password match a user in the array
+        const signIn = (email, password) => {
+            // The array.findIndex() line to search for a user with the index and the provided email
+            const userIndex = allUsers.findIndex(user => user.Email == email)
+        
+            // If a user is found and the password matches, return true
+            if (userIndex !== -1 && allUsers[userIndex].Password == password) {
+                localStorage.setItem('userIndex', userIndex)
+                return true;
+            }
+        
+            // If the user is not found or the password is incorrect, return false
+            return false;
         }
+        
+        const email = userEmail.value
+        const myPassword = userPassword.value
     
-        // If the user is not found or the password is incorrect, return false
-        return false;
-    }
-    
-    const email = userEmail.value
-    const myPassword = userPassword.value
-
-    if (signIn(email, myPassword)) {
-        console.log('Sign-in successful!');
-        swal('Verified', 'You\'re successfully signed in!', 'success')
-        setTimeout(()=> {
-            window.location.href = 'dashboard.html'
-        }, 3000)
+        if (signIn(email, myPassword)) {
+            console.log('Sign-in successful!');
+            swal('Verified', 'You\'re successfully signed in!', 'success')
+            setTimeout(()=> {
+                window.location.href = 'dashboard.html'
+            }, 3000)
+        } else {
+            console.log('Invalid username or password.');
+            swal('Invalid Email/Password!', 'Unable to sign in, kindly fill out the fields correctly!', 'error')
+            setTimeout(()=> {
+                window.location.href = 'signIn.html'
+            }, 3000)
+        }
     } else {
-        console.log('Invalid username or password.');
-        swal('Invalid Email/Password!', 'Unable to sign in, kindly fill out the fields correctly!', 'error')
+        console.log('Account not found! Kindly proceed to sign up.');
         setTimeout(()=> {
-            window.location.href = 'signIn.html'
-        }, 3000)
+            window.location.href = 'signUp.html'
+        }, 2000)
     }
+
 })  
 
   
